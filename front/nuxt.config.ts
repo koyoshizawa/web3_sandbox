4048a95cc5a8034434504f347e88c39b9f967d14
KoYoshizawa <water.9100@gmail.com>
2023-06-06 22:14:46 +0900

    firstcommit

---
+++ front/nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    plugins: [
+        { src: '~/plugins/web3.js', mode: 'client'}
+    ]
+})
---
+++ front/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Main Page</h1>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('current block number')
+        this.$web3.eth.getBlockNumber().then(console.log)
+    }
+}
+
+</script>
