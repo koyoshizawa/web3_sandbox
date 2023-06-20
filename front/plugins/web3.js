dbd07574249cb988a1bd15a276fff85e398c69a5
KoYoshizawa <water.9100@gmail.com>
2023-06-20 22:19:47 +0900

    .

--- front/plugins/web3.js
+++ front/plugins/web3.js
@@ -2,6 +2,7 @@
 import Web3 from "web3"
 
 export default async function(context, inject) {
+    
     const httpEndpoint = "http://127.0.0.1:8545"
     const web3 = new Web3(new Web3.providers.HttpProvider(httpEndpoint))
     inject('web3', web3)
