6579c29e9c22fa70757643f8a7d262bdcc471e55
KoYoshizawa <water.9100@gmail.com>
2023-06-20 21:50:53 +0900

    .

---
+++ chain/contracts/SingleNumRegister.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED 
+
+pragma solidity ^0.8.20;
+
+contract SingleNumRegister {
+    struct StoreNumber {
+        address from;
+        uint256 number;
+    }
+
+    StoreNumber[] public storeNumbers;
+
+    function set(uint256 num) public {
+        storeNumbers.push(StoreNumber(msg.sender, num));
+    }
+
+    function get() public view returns (uint256) {
+        uint256 index = storeNumbers.length - 1;
+        return storeNumbers[index].number;
+    }
+}
