8ec55d9e1cf88dd4abacba4ea599bdf1822db7c1
KoYoshizawa <water.9100@gmail.com>
2023-06-21 21:55:25 +0900

    .

--- front/plugins/web3.js
+++ front/plugins/web3.js
@@ -1,9 +1,26 @@
 // plugins/web3.js
 import Web3 from "web3"
 
+
 export default async function(context, inject) {
-    
-    const httpEndpoint = "http://127.0.0.1:8545"
-    const web3 = new Web3(new Web3.providers.HttpProvider(httpEndpoint))
+    let web3
+
+    if (
+        typeof window !== 'undefined' &&
+        typeof window.ethereum !== 'undefined'
+    ) {
+        web3 = new Web3(window.ethereum)
+        window.ethereum.enable().chatch((error) => {
+            console.log(error)
+        })
+    } else if (
+        typeof window !== 'undefined' &&
+        typeof window.web3 !== 'undefined'
+    ) {
+        web3 = new Web3(window.web3.currentProvider)
+    } else {
+        const httpEndpoint = "http://127.0.0.1:8545"
+        web3 = new Web3(new Web3.providers.HttpProvider(httpEndpoint))
+    }
     inject('web3', web3)
 }
